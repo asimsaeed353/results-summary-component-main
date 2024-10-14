ea0dff6e274bd74d130e09dcac3e046c19a98afc
M Asim Saeed <asimsaeed353@gmail.com>
2024-10-14 16:50:02 +0500

    incorporating fetch api

--- script.js
+++ script.js
@@ -1,29 +1,48 @@
 
-var details = [
-    {
-      "category": "Reaction",
-      "score": 80,
-      "icon": "./assets/images/icon-reaction.svg"
-    },
-    {
-      "category": "Memory",
-      "score": 92,
-      "icon": "./assets/images/icon-memory.svg"
-    },
-    {
-      "category": "Verbal",
-      "score": 61,
-      "icon": "./assets/images/icon-verbal.svg"
-    },
-    {
-      "category": "Visual",
-      "score": 72,
-      "icon": "./assets/images/icon-visual.svg"
-    }
-]
-  
+// var details = [
+//     {
+//       "category": "Reaction",
+//       "score": 80,
+//       "icon": "./assets/images/icon-reaction.svg"
+//     },
+//     {
+//       "category": "Memory",
+//       "score": 92,
+//       "icon": "./assets/images/icon-memory.svg"
+//     },
+//     {
+//       "category": "Verbal",
+//       "score": 61,
+//       "icon": "./assets/images/icon-verbal.svg"
+//     },
+//     {
+//       "category": "Visual",
+//       "score": 72,
+//       "icon": "./assets/images/icon-visual.svg"
+//     }
+// ]
+// function addDetails() {
+//     var clutter = "";
+//     details.forEach(detail => {
+//         clutter += `<div class="stat__info">
+//           <div class="category">
+//           <img src=${detail.icon} alt="${detail.category}-icon">
+//           <p>${detail.category}</p>
+//         </div>
+//           <p class="score"><strong>${detail.score}</strong><span> / 100</span></p>
+//         </div>`
+//     });
 
-function addDetails() {
+//     document.querySelector('.summary__stats').innerHTML = clutter;
+// }
+  
+// addDetails();
+
+/*  Populating Data using Fetch API */
+
+fetch('http://127.0.0.1:5500/data.json')
+  .then((response) => response.json())
+  .then((details) => {
     var clutter = "";
     details.forEach(detail => {
         clutter += `<div class="stat__info">
@@ -36,6 +55,4 @@ function addDetails() {
     });
 
     document.querySelector('.summary__stats').innerHTML = clutter;
-}
-  
-addDetails();
+})
